795743a48e8f81c0cdf0e487882b7a81ff52cc1f
Patrick Bergman <patrick@runline.nl>
2018-08-26 23:00:59 +0200

    Add tests

--- tests/ToolControllerTest.php
+++ tests/ToolControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Runline\ProfileTool\Tests;
 
+use App\User;
 use Runline\ProfileTool\Http\Controllers\ToolController;
 use Runline\ProfileTool\ProfileTool;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,11 +10,63 @@ use Symfony\Component\HttpFoundation\Response;
 class ToolControllerTest extends TestCase
 {
     /** @test */
-    public function it_can_can_return_a_response()
+    public function it_can_can_return_update_user_data()
     {
-        $this->assertTrue(true);
-        // $this
-        //     ->post('nova-vendor/runlinenl/nova-profile-tool')
-        //     ->assertSuccessful();
+        $user = factory(User::class)->create([
+            'name' => 'Someone',
+            'email' => 'someone@example.com'
+        ]);
+
+         $this
+             ->actingAs($user)
+             ->postJson('nova-vendor/runlinenl/nova-profile-tool', ['name' => 'New Name', 'email' => 'other@example.com'])
+             ->assertSuccessful();
+
+         $newUserData = User::get()->first();
+
+         $this->assertEquals('New Name', $newUserData->name);
+         $this->assertEquals('other@example.com', $newUserData->email);
+    }
+
+    /** @test */
+    public function it_requires_a_name()
+    {
+        $user = factory(User::class)->create([
+            'name' => 'Someone',
+            'email' => 'someone@example.com'
+        ]);
+
+        $this
+            ->actingAs($user)
+            ->postJson('nova-vendor/runlinenl/nova-profile-tool', ['email' => 'other@example.com'])
+            ->assertJsonValidationErrors(['name']);
+    }
+
+    /** @test */
+    public function it_requires_an_email()
+    {
+        $user = factory(User::class)->create([
+            'name' => 'Someone',
+            'email' => 'someone@example.com'
+        ]);
+
+        $this
+            ->actingAs($user)
+            ->postJson('nova-vendor/runlinenl/nova-profile-tool', ['name' => 'New Name'])
+            ->assertJsonValidationErrors(['email']);
+    }
+
+    /** @test */
+    public function it_requires_a_valid_email()
+    {
+        $user = factory(User::class)->create([
+            'name' => 'Someone',
+            'email' => 'someone@example.com'
+        ]);
+
+        $this
+            ->actingAs($user)
+            ->postJson('nova-vendor/runlinenl/nova-profile-tool', ['name' => 'New Name', 'email' => 'invalid'])
+            ->assertJsonValidationErrors(['email']);
     }
 }
